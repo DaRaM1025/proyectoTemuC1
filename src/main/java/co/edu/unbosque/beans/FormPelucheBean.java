73926872ca9b5d34264ab7da480eeb5df08e580b
natar2005 <nata.rengifob20@gmail.com>
2025-08-18 14:18:01 -0500

    creacion peluche bean

---
+++ src/main/java/co/edu/unbosque/beans/FormPelucheBean.java
@@ -0,0 +1,11 @@
+package co.edu.unbosque.beans;
+
+import java.io.Serializable;
+
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Named;
+@Named("pelucheBean")
+@ViewScoped
+public class FormPelucheBean implements Serializable{
+
+}
